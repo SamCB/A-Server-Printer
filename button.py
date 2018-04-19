71c18351cbf59ae1827d2ce7814124fd031f8a20
Samuel Collis-Bird <s.cbird01@gmail.com>
2018-04-19 11:35:57 +0000

    Still trying to get button to behave like I want

--- button.py
+++ button.py
@@ -27,15 +27,22 @@ class Button:
         )
 
     def _start_tracking(self, max_time, callback):
+        print("START")
         with self.executor_lock:
+            print("RUNNING?", self.running)
             if self.running:
+                print("ALREADY RUNNING")
                 return
             self.running = True
-        future = self.executor.submit(self._track_button, args=(max_time))
-        future.add_done_callback(lambda f: callback(f.result))
+
+        print(self.running)
+        print("SUBMIT")
+        future = self.executor.submit(self._track_button, max_time)
+        future.add_done_callback(lambda f: callback(f.result()))
         future.add_done_callback(self._finished_tracking)
 
     def _finished_tracking(self, *_):
+        print("FINISHED")
         self.running = False
 
     def _track_button(self, max_time):
@@ -45,9 +52,16 @@ class Button:
                 time.sleep(t/n)
 
         now = time.time()
-        for _ in wait_range(max_time, 0.02):
-            if GPIO.input(self.pin) == 1:
-                break
+        debounce_count = 0
+        for _ in wait_range(max_time, 0.2):
+            val = GPIO.input(self.pin)
+            print("VAL: ", val)
+            if val == 1:
+                if debounce_count == 5:
+                    break
+                debounce_count += 1
+            else:
+                debounce_count = 0
 
         return time.time() - now
 
